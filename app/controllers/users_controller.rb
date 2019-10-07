e525717a82ccc64dcf688882a1c49ff9ab137daa
CrunchyCry <julius.kriesten@gmail.com>
2019-10-07 15:59:58 +0200

    I18n add Deutsch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
         @users = User.all
     end
 
-    
+    def show 
+        @user= User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render "edit"
+        end
+    end
+
+    private
+    def user_params
+        prams.require(:user).permit(:name, :role)
+    end
 
 end
