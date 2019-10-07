47f4d3b1a728ea7ff089eff046fdffbdc5e93589
CrunchyCry <julius.kriesten@gmail.com>
2019-10-07 09:41:11 +0200

    add userrole table only for admin

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  get 'users/index'
+    
+
 end
 
