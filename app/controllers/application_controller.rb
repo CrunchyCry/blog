be0cec4e89f3520ea4d604fb7fac0221f91d93d0
CrunchyCry <julius.kriesten@gmail.com>
2019-10-09 11:33:44 +0200

    fix startup error?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,60 +8,36 @@ class ApplicationController < ActionController::Base
       format.html { redirect_to main_app.root_url, :alert => exception.message }
     end
   end
-
+  
   def default_url_options
     { locale: I18n.locale }
   end
-
-
-  # around_action :switch_locale
-  # def switch_locale(&action)
-  #   locale = params[:locale] || I18n.default_locale
-  #   I18n.with_locale(locale, &action)
-  # end
-
+  
+  around_action :switch_locale
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+  
   protected
-  def set_i18n_locale_from_params
-    if params[:locale]
-      if I18n.available_locales.map(&:to_s).include?(params[:locale])
-        I18n.locale = params[:locale]
-      else
-        I18n.locale = :en
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          I18n.locale = :en
+        end
       end
     end
-  end
+    
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-  end
+    
 
-  aruond_action :switch_locale
-
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale 
-    I18n.with:locale(locale, &action)
-  end
-  
-      # Get locale from top-level domain or return +nil+ if such locale is not available
-    # You have to put something like:
-    #   127.0.0.1 application.com
-    #   127.0.0.1 application.it
-    #   127.0.0.1 application.pl
-    # in your /etc/hosts file to try this out locally
-
-  def extract_locale_from_tld
-    parsed_locale = request.host.split('.').last
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
-  end
-
-  link_to("Deutsch", "#{APP_CONFIG[:deutsch_website_url]}#{request.env['PATH_INFO']}")
-
-  # app/controllers/application_controller.rb
-  def default_url_options
-    { locale: I18n.locale }
-  end
-
-  
-
-
+    def extract_locale_from_tld
+      parsed_locale = request.host.split('.').last
+      I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+    end
 end
